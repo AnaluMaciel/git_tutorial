e417d845cb2f41bf568cffcaec3ce92e7ec0c0ac
AnaluMaciel <rnanalu@yahoo.com.br>
2015-09-10 20:36:19 -0300

    Add an AnaluMaciel/rnanalu@yahoo.com.br comment

--- hello.rb
+++ hello.rb
@@ -4,4 +4,15 @@ puts "Olá, # {name}!"
 
 # O padrao é "World"
 
+# Default is World
+# Author: Jim Weirich
+name = ARGV.first || "World"
 
+puts "Hello, #{name}!"
+
+
+# Default is World
+# Author: Jim Weirich (jim@somewhere.com)
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
